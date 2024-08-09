45b21b8ae0368491843918d9a445165172ff908f
Ian Parkinson <h3lloiain@gmail.com>
2024-08-09 00:10:37 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+Start up various applications on boot. [Systemd](https://systemd.io/)
+configuration in file
+
+## Applications
+
+- ianparkinson.studio
+- animations wiki
+- admin panal
---
+++ start.sh
@@ -0,0 +1,3 @@
+cd /home/ian/admin_panal; pm2 start server.js --name="admin_panal" --watch --ignore-watch="node_modules"
+cd /home/ian/dojo/animations/backEnd; pm2 start server.js --name="animations_wiki" --watch --ignore-watch="node_modules"
+cd /home/ian/ip.studio.prod; pm2 start server.js --name="ianparkinson.studio" --watch --ignore-watch="node_modules"
